e714fa269ad682cc710b8e363439fd145f8627cd
Hilario Arevalo <hilarioarevalo03@gmail.com>
2024-03-28 19:12:03 -0600

    Add component history

---
+++ src/Components/History.jsx
@@ -0,0 +1,18 @@
+export default function History() {
+    return (
+        <>
+            <h1 className="text-center text-5xl text-black font-mono">Our History</h1>
+            <div className="grid text-center place-items-center gap-4 sm:grid-cols-2 mb-8">
+                <div>
+                    <h2 className="text-3xl mb-4">History</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quaerat dolore officia impedit pariatur quas deleniti, at quo provident eius fugit. Officiis eius perspiciatis assumenda fugit dignissimos totam vesit amet consectetur adipisicing elit. Accusamus quaerat dolore officia impedit pariatur quas deleniti, at quo provident eius fugit. Officiis eius perspiciatis assumenda fugit dignissimos totam veniam qorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quaerat dolore officia impedit pariatur quas deleniti, at quo provident eius fugit. Officiis eius perspiciatis assumenda fugit dignissimos totam veniam quidem!</p>
+                    
+                </div>
+                <div>
+                    <h2 className="text-3xl mb-4">Founders</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quaerat dolore officia impedit pariatur quas deleniti, at quo provident eius fugit. Officiis eius perspiciatis assumenda fugit dignissimos totam vesit amet consectetur adipisicing elit. Accusamus quaerat dolore officia impedit pariatur quas deleniti, at quo provident eius fugit. Officiis eius perspiciatis assumenda fugit dignissimos totam veniam qorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quaerat dolore officia impedit pariatur quas deleniti, at quo provident eius fugit. Officiis eius perspiciatis assumenda fugit dignissimos totam veniam quidem!</p>
+                </div>
+            </div>
+        </>
+    )
+}
