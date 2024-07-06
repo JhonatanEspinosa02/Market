59e1f24fc31879339427af0cc2b6ae56f259bb07
Arevted23 <hilarioarevalo03@gmail.com>
2024-07-06 00:08:38 -0600

    add file locals what contain routes images and library of content

---
+++ locales/locales_app.jsx
@@ -0,0 +1,32 @@
+const LOCALES = {
+  IMAGES: {
+    apple: require("./../../../assets/download-on-the-app-store.png"),
+    google: require("./../../../assets/google-play-badge.png"),
+    masterCard: require("./../../../assets/mastercard-icon-1024x643-j3zb44jj.png"),
+  },
+  FOOTER: {
+    footerAboutOurApp: "About of our app",
+    footerDescriptionApp:
+      "Add a little description about our app in IOS and ANDROID",
+    footerReservedRight:
+      "©2024 XpressMarket All rights reserved & copyright reserved for developers",
+    footerGetConnected: "Get Connected",
+    footerBlog: "Blog",
+    footerPricing: "Pricing",
+    footerWhyXpressM: "Why XpressMarket?",
+    footerSupport: "Support",
+    footerStore: "Store",
+    footerCommunity: "Community",
+    footerHelpCenter: "Help Center",
+    footerCompany: "Company",
+    footerJobs: "Jobs",
+    footerParterships: "Parterships",
+    footerPoliticsPrivaci: "Politics & Privaci",
+    footerSocialMedia: "Social",
+    footerSocialMediaX: "X",
+    footerSocialMediaFacebook: "Facebook",
+    footerSocialMediaInstagram: "Instagram",
+  },
+};
+
+export default LOCALES;
