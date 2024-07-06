ecf8509d3798a6eb7fd4715461aefb9c3c1bc818
Arevted23 <hilarioarevalo03@gmail.com>
2024-07-06 00:08:57 -0600

    Refactor code in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
-// import Promotion from "./Components/Promotions";
-// import Navigation from "./Components/Navigation";
-// import EatClean from "./Components/EatClean";
-import Header from "./Components/MarketComponentsNative/MarketHeader/Header"
-import TopProducts from "./Components/MarketComponentsNative/MarketProducts/TopProducts"
-import ShopCategory from "./Components/MarketComponentsNative//MarketShop/ShopByCate"
-import Score from "./Components/MarketComponentsNative/MarketScore/Score"
-import News from "./Components/MarketComponentsNative/MarketNews/SectionNews"
-import Footer from "./Components/MarketComponentsNative/MarketFooter/Footer"
-
-
+//Import main pages in this file
+import { Home } from "./Pages/Home/home"
 
 function App() {
   return (
     <>
-      {/* I delete four components and traspase a only just one component her name Header, the files are in trash */}
-      <Header />
-      <TopProducts />
-      <ShopCategory />
-      <Score/>
-      <News/>
-      <Footer/>
-
+      <Home/>
     </>
   )
 }
