d7d9ca63450342b65650750253315ccfab46f873
Arevted23 <hilarioarevalo03@gmail.com>
2024-07-06 00:10:21 -0600

    transfer app to home page

---
+++ src/Pages/Home/home.jsx
@@ -0,0 +1,20 @@
+//Import Components
+import Header from "../../Components/MarketComponentsNative/MarketHeader/Header";
+import TopProducts from "../../Components/MarketComponentsNative/MarketProducts/TopProducts";
+import ShopCategory from "../../Components/MarketComponentsNative/MarketShop/ShopByCate";
+import Score from "../../Components/MarketComponentsNative/MarketScore/Score";
+import News from "../../Components/MarketComponentsNative/MarketNews/SectionNews";
+import Footer from "../../Components/MarketReutil/MarketFooter/Footer";
+
+export function Home() {
+  return (
+    <>
+      <Header />
+      <TopProducts />
+      <ShopCategory />
+      <Score />
+      <News />
+      <Footer />
+    </>
+  );
+}
