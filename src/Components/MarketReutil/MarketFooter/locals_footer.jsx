fb86000eb2353feb49efe6347df8c8f294ede1b0
Arevted23 <hilarioarevalo03@gmail.com>
2024-07-08 21:34:25 -0600

    new imports in locals footer

---
+++ src/Components/MarketReutil/MarketFooter/locals_footer.jsx
@@ -0,0 +1,32 @@
+import {appleImage, googleImage, masterCardImage} from '../../../../locales/imports_images'
+
+export const LOCALS_FOOTER = {
+IMAGES: {
+  apple: {appleImage},
+  google: googleImage,
+  masterCard: masterCardImage,
+},
+  FOOTER: {
+    footerAboutOurApp: "About of our app",
+    footerDescriptionApp:
+      "Add a little description about our app in IOS and ANDROID",
+    footerReservedRight:
+      "©2024 XpressMarket All rights reserved & copyright reserved for developers",
+    footerGetConnected: "Get Connected",
+    footerBlog: "Blog",
+    footerPricing: "Pricing",
+    footerWhyXpressM: "Why XpressMarket?",
+    footerSupport: "Support",
+    footerStore: "Store",
+    footerCommunity: "Community",
+    footerHelpCenter: "Help Center",
+    footerCompany: "Company",
+    footerJobs: "Jobs",
+    footerParterships: "Parterships",
+    footerPoliticsPrivaci: "Politics & Privaci",
+    footerSocialMedia: "Social",
+    footerSocialMediaX: "X",
+    footerSocialMediaFacebook: "Facebook",
+    footerSocialMediaInstagram: "Instagram",
+  },
+};
