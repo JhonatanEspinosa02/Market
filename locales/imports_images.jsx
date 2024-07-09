8e3dfd3cf5361059aba9c9060cd9550fb9ca9122
Arevted23 <hilarioarevalo03@gmail.com>
2024-07-08 21:31:08 -0600

    new file what contain url of images and export the same

---
+++ locales/imports_images.jsx
@@ -0,0 +1,9 @@
+import appleImage from './../src/assets/download-on-the-app-store.png';
+import googleImage from './../src/assets/google-play-badge.png';
+import masterCardImage from './../src/assets/mastercard-icon-1024x643-j3zb44jj.png';
+
+export {
+    appleImage,
+    googleImage,
+    masterCardImage
+}
