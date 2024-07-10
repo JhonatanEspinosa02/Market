6c35666474bdde3c04efc0eb63879f9c341e83a6
Arevted23 <hilarioarevalo03@gmail.com>
2024-07-09 20:10:32 -0600

    add commit into local images

--- locales/imports_images.jsx
+++ locales/imports_images.jsx
@@ -1,3 +1,4 @@
+//Import and export name and routes of images
 import appleImage from './../src/assets/download-on-the-app-store.png';
 import googleImage from './../src/assets/google-play-badge.png';
 import masterCardImage from './../src/assets/mastercard-icon-1024x643-j3zb44jj.png';
