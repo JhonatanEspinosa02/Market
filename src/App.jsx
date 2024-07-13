c3825c4d4a80d2b49a3ebf26b2c9c92216d853e4
Hilario Arevalo <hilarioarevalo03@gmail.com>
2024-07-13 11:35:59 -0600

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
+import { Routes, Route } from "react-router-dom";
 //Import main pages in this file
-import HeaderNavigation from "./Components/MarketReutil/navigation/header_navigation"
-import { About } from "./Pages/About/about"
-import { Browse } from "./Pages/Browse/browse"
-import { Home } from "./Pages/Home/home"
+import HeaderNavigation from "./Components/MarketReutil/navigation/header_navigation";
+
+//Navigations pages
+import { About } from "./Pages/About/about";
+import { Browse } from "./Pages/Browse/browse";
+import { Home } from "./Pages/Home/home";
 
 function App() {
   return (
     <>
-    {/* By default the navigation here */}
-    <HeaderNavigation></HeaderNavigation>
-    {/* By default pages in navigation call */}
-      <Home/> 
-      {/* <Browse></Browse>
-      <About></About> */}
+      {/* By default the navigation here */}
+      <Routes>
+        <Route path="/" element={<HeaderNavigation></HeaderNavigation>}>
+          <Route path="/home" element={<Home></Home>}></Route>
+          <Route path="/about" element={<About></About>}></Route>
+          <Route path="/browse" element={<Browse></Browse>}></Route>
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
