41e4eda1ae2181d2e7721bb950148f39867cb40b
Hilario Arevalo <hilarioarevalo03@gmail.com>
2024-07-12 22:10:20 -0600

    adding new components in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 //Import main pages in this file
+import HeaderNavigation from "./Components/MarketReutil/navigation/header_navigation"
+import { About } from "./Pages/About/about"
+import { Browse } from "./Pages/Browse/browse"
 import { Home } from "./Pages/Home/home"
 
 function App() {
   return (
     <>
-      <Home/>
+    {/* By default the navigation here */}
+    <HeaderNavigation></HeaderNavigation>
+    {/* By default pages in navigation call */}
+      <Home/> 
+      {/* <Browse></Browse>
+      <About></About> */}
     </>
   )
 }
