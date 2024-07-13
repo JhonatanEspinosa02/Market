e59dd817120d84bc82cd4b760b860dafaeed418a
Hilario Arevalo <hilarioarevalo03@gmail.com>
2024-07-12 22:18:30 -0600

    deleting component navigation

--- src/Components/MarketComponentsNative/HEADER/MarketHeader/Header.jsx
+++ src/Components/MarketComponentsNative/HEADER/MarketHeader/Header.jsx
@@ -2,12 +2,10 @@ import React from "react";
 
 //import custom components
 import SpanNavigation from "./span/header_span";
-import HeaderNavigation from "./navigation/header_navigation";
 
 export default function Header() {
   return (
     <>
-      <HeaderNavigation></HeaderNavigation>
       <SpanNavigation></SpanNavigation>
     </>
   );
