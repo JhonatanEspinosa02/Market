8aaf502704183d17ec66b71aa67825381df4d6e4
Hilario Arevalo <hilarioarevalo03@gmail.com>
2024-07-14 19:14:37 -0600

    adding new routes of navigation

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import HeaderNavigation from "./Components/MarketReutil/navigation/header_naviga
 import { About } from "./Pages/About/about";
 import { Browse } from "./Pages/Browse/browse";
 import { Home } from "./Pages/Home/home";
+import PresentationPageMain from "./Pages/PresentationPage/Presentation";
+import OrdersUser from "./Pages/Orders/orders";
+import CustomerLogin from "./Pages/Auth/LoginCustomer/customer";
+import SellerLogin from "./Pages/Auth/LoginSeller/seller";
 
 function App() {
   return (
@@ -13,9 +17,13 @@ function App() {
       {/* By default the navigation here */}
       <Routes>
         <Route path="/" element={<HeaderNavigation></HeaderNavigation>}>
+        <Route path="/" element={<PresentationPageMain></PresentationPageMain>}></Route>
           <Route path="/home" element={<Home></Home>}></Route>
-          <Route path="/about" element={<About></About>}></Route>
+          <Route path="/orders" element={<OrdersUser></OrdersUser>}></Route>
           <Route path="/browse" element={<Browse></Browse>}></Route>
+          <Route path="/about" element={<About></About>}></Route>
+          <Route path="/customer-login" element={<CustomerLogin></CustomerLogin>}></Route>
+          <Route path="/seller-login" element={<SellerLogin></SellerLogin>}></Route>
         </Route>
       </Routes>
     </>
