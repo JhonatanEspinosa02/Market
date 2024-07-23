13b641f726008325b2237edf206a86f3ffcd4d53
Hilario Arevalo <hilarioarevalo03@gmail.com>
2024-07-23 10:25:15 -0600

    prettier formatier in presentation

--- front-end/src/Pages/PresentationPage/presentation.jsx
+++ front-end/src/Pages/PresentationPage/presentation.jsx
@@ -1,14 +1,14 @@
 import Input from "./inputLocation/input_location";
 import MostRequested from "./mostRequested/mostRequested";
-import InfoApp from "./infoApp/infoApp"
-import './styles.css'
+import InfoApp from "./infoApp/infoApp";
+import "./styles.css";
 
-export default function PresentationPageMain(){
-    return(
-        <>
-            <Input></Input>
-            <MostRequested></MostRequested>
-            <InfoApp></InfoApp>
-        </>
-    )
+export default function PresentationPageMain() {
+  return (
+    <>
+      <Input></Input>
+      <MostRequested></MostRequested>
+      <InfoApp></InfoApp>
+    </>
+  );
 }
